b74a5e568b5afd402519730a2ca6cc4f4da8277b
Giuseppe Verni <gverni@twilio.com>
2019-09-23 12:53:27 +0100

    Simplify adding the local video track
    In this alternative approach, generate am array of localVideoTrack
    and pass the array in the connection options for the Video.connect()

--- public/client/client.js
+++ public/client/client.js
@@ -4,28 +4,66 @@ localTracks = [];
 var clientToken;
 var localDataTrack;
 
-async function getVideoDevicesMediaStreamTracks() {
-  // To make this code more readable, this is an async function
-  // In production promises can be used instead for wider compatibility
-  let mediaStreamTracks = [];
-  let deviceInfos = await navigator.mediaDevices.enumerateDevices();
-  let videoDeviceInfos = deviceInfos.filter(
-    deviceInfo => deviceInfo.kind === 'videoinput'
-  );
+// This is an example of how to create a mediaStreamTracks array from all video sources
+// This is an alternative to use getUserMedia() to generate a list of tracks
+// (see examples in https://media.twiliocdn.com/sdk/js/video/releases/1.0.0/docs/global.html#connect)
+// Using getUserMedia() infact just generate an audio track and the defaul video track
+//
+// async function getVideoDevicesMediaStreamTracks() {
+//   // To make this code more readable, this is an async function
+//   // In production promises can be used instead for wider compatibility
+//   let mediaStreamTracks = [];
+//   let deviceInfos = await navigator.mediaDevices.enumerateDevices();
+//   let videoDeviceInfos = deviceInfos.filter(
+//     deviceInfo => deviceInfo.kind === 'videoinput'
+//   );
 
-  // To make this code more readable, we hardcode the two cameras
-  // Production code can use Promise.all() to create one single promise
-  let camera1 = await navigator.mediaDevices.getUserMedia({
-    video: { deviceId: videoDeviceInfos[0].deviceId },
-    audio: false
-  });
-  let camera2 = await navigator.mediaDevices.getUserMedia({
-    video: { deviceId: videoDeviceInfos[1].deviceId },
-    audio: false
-  });
-  mediaStreamTracks.push(camera1.getTracks()[0]);
-  mediaStreamTracks.push(camera2.getTracks()[0]);
-  return mediaStreamTracks;
+//   // To make this code more readable, we hardcode the two cameras
+//   // Production code can use Promise.all() to create one single promise
+//   let camera1 = await navigator.mediaDevices.getUserMedia({
+//     video: { deviceId: videoDeviceInfos[0].deviceId },
+//     audio: false
+//   });
+//   let camera2 = await navigator.mediaDevices.getUserMedia({
+//     video: { deviceId: videoDeviceInfos[1].deviceId },
+//     audio: false
+//   });
+//   mediaStreamTracks.push(camera1.getTracks()[0]);
+//   mediaStreamTracks.push(camera2.getTracks()[0]);
+//   return mediaStreamTracks;
+// }
+
+// This is an example of using the above function
+// function connect() {
+//   ...
+//   getVideoDevicesMediaStreamTracks().then(videoDevicesMediaStreamTracks => {
+//     // For demo pourpose, we don't use any audio (to avoid echo)
+//     var connectOptions = {
+//       name: urlParams.get('room'),
+//       tracks: videoDevicesMediaStreamTracks
+//     };
+//     Video.connect(token.token, connectOptions).then(roomJoined, function(
+//       error
+//     ) {
+//       log('Could not connect to Twilio: ' + error.message);
+//     });
+//   });
+//   ...
+// }
+
+// This is a _simplified_ version of generating as many local video tracks
+// as available devices. For a different approach to that see the gist above
+function getAvailableLocalVideoTracks() {
+  return navigator.mediaDevices.enumerateDevices()
+  .then(inputDeviceInfos => {
+    let promises = [] 
+     inputDeviceInfos.forEach(inputDeviceInfo => {
+       if (inputDeviceInfo.kind == 'videoinput') {
+        promises.push(Video.createLocalVideoTrack({deviceId: inputDeviceInfo.deviceId}))
+       }
+     })
+     return Promise.all(promises)
+  })
 }
 
 function createDataTrack(room) {
@@ -42,19 +80,20 @@ function roomJoined(room) {
 document.getElementById('connect').onclick = function(event) {
   fetchToken().then(token => {
     clientToken = token;
-    getVideoDevicesMediaStreamTracks().then(videoDevicesMediaStreamTracks => {
-      // For demo pourpose, we don't use any audio (to avoid echo)
-      var connectOptions = {
+    getAvailableLocalVideoTracks().then(availableLocalVideoTracks => {
+      connectOptions = {
         name: urlParams.get('room'),
-
-        tracks: videoDevicesMediaStreamTracks
+        // It's a demo, we don't want an echo
+        // Remove below line if you want audio track as well (I warned you ;) )
+        audio: false,
+        tracks: availableLocalVideoTracks
       };
       Video.connect(token.token, connectOptions).then(roomJoined, function(
         error
       ) {
         log('Could not connect to Twilio: ' + error.message);
       });
-    });
+    })
   });
 };
 
@@ -66,6 +105,6 @@ window.ondeviceorientation = function(event) {
   }
 };
 
-document.getElementById('cancle').onclick = function () {
-  document.getElementById('confirm-screen').style.display = hidden
-}
+document.getElementById('cancel').onclick = function() {
+  document.getElementById('confirm-screen').style.display = hidden;
+};
